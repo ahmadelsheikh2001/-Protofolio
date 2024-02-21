18ef2c70b51eb609815a642ce046c265a61a0a4b
khaled975 <khaledbebo975@gmail.com>
2024-02-21 22:34:53 +0200

    add dashboard navbar

--- src/layout/admin/AdminLayout.jsx
+++ src/layout/admin/AdminLayout.jsx
@@ -1,32 +1,37 @@
-import React, { useEffect } from 'react'
-import AdminProvider from '../../store/AdminProvider';
-import AdminContent from './AdminContent';
-import Sidebar from './Sidebar';
+import React, { useEffect } from "react";
+import AdminProvider from "../../store/AdminProvider";
+import AdminContent from "./AdminContent";
+import Sidebar from "./Sidebar";
 
-import './dashLayout.css';
-import { useSelector } from 'react-redux';
-import Login from '../../Pages/admin/login/Login';
-import { Outlet } from 'react-router-dom';
-
+import "./dashLayout.css";
+import { useSelector } from "react-redux";
+import Login from "../../Pages/admin/login/Login";
+import { Navigate, Outlet } from "react-router-dom";
+import AppProvider from "../../store/AppProvider";
 
 const AdminLayout = () => {
-  const authUser = useSelector((state) => state.user.logedin)
+  const authUser = useSelector((state) => state.user.logedin);
   useEffect(() => {
-    document.dir = 'rtl';
-    document.getElementsByTagName('html')[0].setAttribute("lang", "ar");
-  }, [])
+    document.dir = "rtl";
+    document.getElementsByTagName("html")[0].setAttribute("lang", "ar");
+  }, []);
 
   return (
     <>
-      {authUser ? <AdminProvider>
-        <div className='admin_layout'>
-          <Sidebar />
-          <AdminContent />
-        </div>
-      </AdminProvider> :
-        <Outlet />
-      }
-    </>)
-}
+      <AppProvider>
+        {authUser ? (
+          <AdminProvider>
+            <div className="admin_layout">
+              <Sidebar />
+              <AdminContent />
+            </div>
+          </AdminProvider>
+        ) : (
+          <Navigate to="/admin/login" />
+        )}
+      </AppProvider>
+    </>
+  );
+};
 
-export default AdminLayout
+export default AdminLayout;
