b4d4d36fb235782feaefcdeabd8b59a022691bd9
ahmadelsheikhali2001 <ahmadelsheikhali@gmail.com>
2024-02-29 23:27:09 +0200

    dashpord badrk

--- src/components/admin/Feedbacks.jsx
+++ src/components/admin/Feedbacks.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
 import { NavLink } from 'react-router-dom';
 import FeedbacksTable from './tables/FeedbacksTable';
 import AdminCards from '../../UI/Cards/AdminCards';
+import AppContext from '../../store/app-context';
+import React, { useContext } from 'react';
 
 
 const Feedbacks = ({ feedbacks }) => {
   
+  const ctx = useContext(AppContext);
 
+  const themeClass = ctx.theme === 'dark' ? 'dark-mode' : '';
   return (
-    <AdminCards>
-      <div className=' oreder_container_header flex'>
-        <h3 className='title'>أراء الناس</h3>
+    <AdminCards  >
+      <div className={`order-card ${themeClass} oreder_container_header flex`}>
+
+        <h3 className='order-title '>أراء الناس</h3>
         <NavLink to='allfeedbacks'>عرض الكل</NavLink>
       </div>
       <FeedbacksTable currentData={feedbacks}/>
--- src/components/admin/Orders.jsx
+++ src/components/admin/Orders.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import { NavLink } from "react-router-dom";
 import AdminCards from '../../UI/Cards/AdminCards';
 import OrederTable from './tables/OrederTable';
+import AppContext from '../../store/app-context';
+import './order.css';
 
 
 const Orders = ({ orders }) => {
+  const ctx = useContext(AppContext);
+
+  const themeClass = ctx.theme === 'dark' ? 'dark-mode' : '';
 
   return (
-    <AdminCards>
-      <div className=' oreder_container_header flex'>
-        <h3 className='title'>طلبات العملاء</h3>
+    <AdminCards >
+      <div className={`order-card ${themeClass} oreder_container_header flex`}>
+        <h3 className=' order-title'>طلبات العملاء</h3>
         <NavLink to='requests'>عرض الكل</NavLink>
       </div>
       <OrederTable currentData={orders}/>
